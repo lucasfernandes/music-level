0d94fec112478d8b77434776a5c9291669da4795
Lucas Silveira <lucas.silveira@ilia.digital (SIS-7588)>
2021-03-12 18:21:53 -0300

    project default structure + AppIcon + LaunchScreen

---
+++ Shared/MusicLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLevelApp.swift
+//  Shared
+//
+//  Created by Lucas Silveira on 12/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
